791e150b358f645e0d872dc7a2ceee0934be7ac2
Derek Wheelden <derek.wheelden@gmail.com>
2025-01-12 23:43:18 -0500

    Implement timers and animations

--- src/config.h
+++ src/config.h
@@ -7,6 +7,12 @@
 
 // Timer configuration
 #define POMODORO_CYCLES 4
-#define POMODORO_WORK_MINS 25
+#define POMODORO_WORK_MINS 20
 #define POMODORO_REST_MINS 5
 #define POMODORO_BREAK_MINS 20
+
+// Animation configuration
+#define CHASE_DELAY_MS 1000 / 30
+#define CHASE_TAIL_LENGTH 8
+#define CLOCK_DELAY_MS 1000 / 30
+#define CLOCK_PULSE_MS 1000
--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,16 @@ u_long pomodoroStartMs;
 int pomodoroMode = INACTIVE;
 int pomodoroCycle = 0;
 int lastPixel = NUM_PIXELS;
+u_long lastButtonPress;
 
 // Function definitions
 void initializePixels();
 void updatePomodoroCycle(u_long ms);
 void updateLedRing(u_long ms);
-void handleButtonPress();
+void handleButtonPress(u_long ms);
 void updateAnimation(u_long ms);
-float breath(int value, int timeFactor = 1);
+void chase(int colors[4]);
+void clock(int colors[4], long elapsedMs, int totalMins);
 
 void setup()
 {
@@ -40,39 +42,51 @@ void setup()
     initializePixels();
 }
 
+u_long lastLoop = 0;
+
 void loop()
 {
     u_long currentMs = millis();
-    // initializePixels();
+
+    if (mainButton.pressed())
+    {
+        lastButtonPress = currentMs;
+    }
 
     if (mainButton.released())
     {
         Serial.println("Button released");
-        handleButtonPress();
+        handleButtonPress(currentMs);
+        lastButtonPress = -1;
     }
 
-    if (pomodoroMode != INACTIVE)
+    if (currentMs - lastLoop > 10)
     {
-        updatePomodoroCycle(currentMs);
-        updateLedRing(currentMs);
+        if (pomodoroMode != INACTIVE)
+        {
+            updatePomodoroCycle(currentMs);
+            updateLedRing(currentMs);
+        }
+
+        updateAnimation(currentMs);
+
+        pixels.show();
+
+        lastLoop = currentMs;
     }
-
-    updateAnimation(currentMs);
-
-    pixels.show();
 }
 
-void handleButtonPress()
+void handleButtonPress(u_long ms)
 {
     if (pomodoroMode == INACTIVE)
     {
         pomodoroMode = WORK;
-        pomodoroStartMs = 0;
+        pomodoroStartMs = ms;
     }
     else
     {
         // This will force `updatePomodoroCycle` to the next cycle
-        pomodoroStartMs = -999999;
+        pomodoroStartMs = -999999999;
     }
 }
 
@@ -119,10 +133,6 @@ void updateLedRing(u_long ms)
         factor = POMODORO_BREAK_MINS * 60000 / NUM_PIXELS;
         lastPixel = floor(elapsedMs / factor);
     }
-    else
-    {
-        // Fail state
-    }
 
     lastPixel = lastPixel == 0 ? 1 : lastPixel;
 }
@@ -131,7 +141,7 @@ void initializePixels()
 {
     for (int i = 0; i < NUM_PIXELS; i++)
     {
-        pixels.setPixelColor(i, pixels.Color(0, (1.0 * i / NUM_PIXELS) * 50, (1.0 * i / NUM_PIXELS) * 50, 0));
+        pixels.setPixelColor(i, 0);
     }
 
     pixels.show();
@@ -139,56 +149,108 @@ void initializePixels()
 
 void updateAnimation(u_long ms)
 {
-    pixels.clear();
+    long elapsedMs = (ms - pomodoroStartMs);
 
     if (pomodoroMode == INACTIVE)
     {
-        for (int i = 0; i < NUM_PIXELS; i++)
-        {
-            pixels.setPixelColor(i, pixels.Color(0, breath(50), breath(50), 0));
-        }
+        int colors[4] = {0, 0, 0, 100};
+
+        chase(colors);
     }
     else if (pomodoroMode == WORK)
     {
-        for (int i = 0; i < lastPixel; i++)
-        {
-            pixels.setPixelColor(i, pixels.Color(breath(50), 0, 0, 0));
-        }
+        int colors[4] = {100, 0, 20, 0};
+
+        clock(colors, elapsedMs, POMODORO_WORK_MINS);
     }
     else if (pomodoroMode == REST)
     {
-        for (int i = 0; i < lastPixel; i++)
-        {
-            pixels.setPixelColor(i, pixels.Color(0, 0, breath(50), 0));
-        }
+        int colors[4] = {0, 20, 100, 0};
+
+        clock(colors, elapsedMs, POMODORO_REST_MINS);
     }
     else if (pomodoroMode == BREAK)
     {
-        for (int i = 0; i < lastPixel; i++)
-        {
-            pixels.setPixelColor(i, pixels.Color(0, breath(50), 0, 0));
-        }
+        int colors[4] = {0, 100, 20, 0};
+
+        clock(colors, elapsedMs, POMODORO_BREAK_MINS);
     }
 }
 
-int brightness = 0;
-int direction = 1;
+long lastMoveMs = -1;
+int nextPosition = 0;
 
-float breath(int value, int timeFactor)
+void chase(int colors[4])
 {
-    int range = 32768 * timeFactor;
+    u_long currentMs = millis();
 
-    brightness = brightness + direction;
-    if (brightness >= range)
+    if (currentMs - lastMoveMs > CHASE_DELAY_MS)
     {
-        direction = -1;
+        // Turn off the tail pixel that's furthest back
+        int tailEndIndex = (nextPosition - CHASE_TAIL_LENGTH + NUM_PIXELS) % NUM_PIXELS;
+        pixels.setPixelColor(tailEndIndex, 0);
+
+        // Set the current head pixel
+        pixels.setPixelColor(nextPosition, pixels.Color(colors[0], colors[1], colors[2], colors[3]));
+
+        // Update the tail pixels
+        for (int i = 1; i < CHASE_TAIL_LENGTH; ++i)
+        {
+            int tailIndex = (nextPosition - i + NUM_PIXELS) % NUM_PIXELS;
+            float scale = 1.0 * (CHASE_TAIL_LENGTH - i) / CHASE_TAIL_LENGTH;
+
+            // Precompute scaled colors
+            uint8_t scaledR = colors[0] * scale;
+            uint8_t scaledG = colors[1] * scale;
+            uint8_t scaledB = colors[2] * scale;
+            uint8_t scaledW = colors[3] * scale;
+
+            pixels.setPixelColor(tailIndex, pixels.Color(scaledR, scaledG, scaledB, scaledW));
+        }
+
+        // Update position and timing
+        lastMoveMs = currentMs;
+        nextPosition = (nextPosition + 1) % NUM_PIXELS;
     }
-    if (brightness <= 0)
+}
+
+long clockLastMoveMs = -1;
+long clockPulseStep = 0;
+float clockPulseSteps = CLOCK_PULSE_MS / CLOCK_DELAY_MS;
+
+void clock(int colors[4], long elapsedMs, int totalMins)
+{
+    long clockMs = millis();
+    float timePerPixelMs = (1.0f * totalMins / NUM_PIXELS) * 60000.0;
+
+    if (clockMs - clockLastMoveMs > CLOCK_DELAY_MS)
     {
-        direction = 1;
+        pixels.clear();
+
+        int lastPixelIndex = ceil(elapsedMs / timePerPixelMs);
+
+        for (int i = 0; i < lastPixelIndex; ++i)
+        {
+            float scale = 0.1;
+            float scaledR = colors[0] * scale;
+            float scaledG = colors[1] * scale;
+            float scaledB = colors[2] * scale;
+            float scaledW = colors[3] * scale;
+
+            pixels.setPixelColor(i, pixels.Color(scaledR, scaledG, scaledB, scaledW));
+        }
+
+        // Pulse the lead pixel
+        float pulseScale = abs(clockPulseStep - (CLOCK_PULSE_MS / 2.0)) / (CLOCK_PULSE_MS / 2.0);
+        float pulsedR = colors[0] * pulseScale;
+        float pulsedG = colors[1] * pulseScale;
+        float pulsedB = colors[2] * pulseScale;
+        float pulsedW = colors[3] * pulseScale;
+
+        pixels.setPixelColor(lastPixelIndex - 1, pixels.Color(pulsedR, pulsedG, pulsedB, pulsedW));
+
+        // Prepare next step
+        clockPulseStep = clockPulseStep + CLOCK_DELAY_MS > CLOCK_PULSE_MS ? 0 : clockPulseStep + CLOCK_DELAY_MS;
+        clockLastMoveMs = clockMs;
     }
-
-    float scalar = 1.0 * brightness / range;
-
-    return value * scalar;
 }
